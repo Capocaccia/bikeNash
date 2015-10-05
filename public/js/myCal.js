69f0fc831a1c8c84a8e532c6d311a995150f886d
Carter Capocaccia <carter.capocaccia@gmail.com>
2015-10-05 11:35:41 -0500

    added dummy data for events

--- public/js/myCal.js
+++ public/js/myCal.js
@@ -3,7 +3,18 @@ $(document).ready(function() {
     // page is now ready, initialize the calendar...
 
     $('#calendar').fullCalendar({
-        // put your options and callbacks here
+         events: [
+        {
+            title  : 'event2',
+            start  : '2015-10-05',
+            end    : '2015-10-6'
+        },
+        {
+            title  : 'event3',
+            start  : '2015-10-09T12:30:00',
+            allDay : false // will make the time show
+        }
+    ]
     })
 
 });
