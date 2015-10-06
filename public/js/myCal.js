e78ef4c3f974a27694bc5dbac584fbd7da9c5e90
Carter Capocaccia <carter.capocaccia@gmail.com>
2015-10-06 10:39:02 -0500

    added click events for calendar events

--- public/js/myCal.js
+++ public/js/myCal.js
@@ -7,14 +7,28 @@ $(document).ready(function() {
         {
             title  : 'event2',
             start  : '2015-10-05',
-            end    : '2015-10-6'
+            end    : '2015-10-08',
+            url: 'http://google.com/'
+        },
+        {
+            title  : 'event2.1',
+            start  : '2015-10-05',
+            end    : '2015-10-08',
+            url: 'http://google.com/'
         },
         {
             title  : 'event3',
             start  : '2015-10-09T12:30:00',
             allDay : false // will make the time show
         }
-    ]
+    ],
+    eventClick: function(event) {
+        if (event.url) {
+            window.open(event.url);
+            return false;
+        }
+    }
+
     })
 
 });
